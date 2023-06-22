b24c5aaa6808211ed22967005d498682f9bd9e8f
Carl Orale <oralecarlangelo@gmail.com>
2023-06-23 03:47:15 +0800

    updated

--- README.md
+++ README.md
@@ -22,68 +22,39 @@ Here's an example of implementing routing in a React application using React Rou
 
 ```javascript
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useHistory,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-function Home() {
-  return <h1>Welcome to the Home page!</h1>;
-}
+// Components for different routes
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Contact = () => <h1>Contact</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
 
-function About() {
-  return <h1>About Us</h1>;
-}
-
-function UserProfile() {
-  const { username } = useParams();
-  return <h1>User Profile: {username}</h1>;
-}
-
-function NotFound() {
-  return <h1>404 Not Found</h1>;
-}
-
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/user/johndoe">User Profile</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/user/:username">
-            <UserProfile />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
 ```
--- src/App.js
+++ src/App.js
@@ -1,69 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link, useHistory, useParams } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-function Home() {
-  return <h1>Welcome to the Home page!</h1>;
-}
+// Components for different routes
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Contact = () => <h1>Contact</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
 
-function About() {
-  return <h1>About Us</h1>;
-}
-
-function UserProfile() {
-  const { username } = useParams();
-  return <h1>User Profile: {username}</h1>;
-}
-
-function NotFound() {
-  const history = useHistory();
-
-  const goBack = () => {
-    history.goBack();
-  };
-
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-      <button onClick={goBack}>Go Back</button>
-    </div>
-  );
-}
-
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/user/johndoe">User Profile</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/user/:username">
-            <UserProfile />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
