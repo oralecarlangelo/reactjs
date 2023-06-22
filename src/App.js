1adac7ad8a312cfe6956b583e0c8106a7b9cabd0
Carl Orale <oralecarlangelo@gmail.com>
2023-06-23 04:27:08 +0800

    fix: directory

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import ExpensiveComponent from "../ExpensiveComponent";
-import ParentMemoComponent from "../MemoizedComponent";
-import ParentComponent from "../ParentComponent";
+import { useState, useEffect } from 'react';
+
+function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+}
 
 function App() {
+  const windowWidth = useWindowWidth();
+
   return (
     <div>
-      <ExpensiveComponent />
-      <ParentComponent />
-      <ParentMemoComponent />
+      <p>Window Width: {windowWidth}px</p>
     </div>
   );
 }
